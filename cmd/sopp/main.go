db8ada1b627605930f93416f4a429d31865e1e8e
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-01-15 05:14:29 +0100

    Add flag to dump db to stdout

--- cmd/sopp/main.go
+++ cmd/sopp/main.go
@@ -17,6 +17,7 @@ func main() {
 
 	importF := flag.String("i", "", "import nt/ttl to db")
 	baseURI := flag.String("base", "http://localhost/", "base URI")
+	dump := flag.Bool("d", false, "dump database as turtle to standard out")
 
 	flag.Usage = func() {
 		fmt.Fprintln(os.Stderr, "Usage: sopp <flags> <database file>")
@@ -25,7 +26,7 @@ func main() {
 
 	flag.Parse()
 
-	if len(flag.Args()) < 1 || *importF == "" || *baseURI == "" {
+	if len(flag.Args()) < 1 || *baseURI == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -36,14 +37,22 @@ func main() {
 	}
 	defer db.Close()
 
-	rdf, err := os.Open(*importF)
-	if err != nil {
-		log.Fatal(err)
+	if *importF != "" {
+		rdf, err := os.Open(*importF)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		n, err := db.Import(rdf, importBatchSize)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("imported %d triples from %s", n, *importF)
 	}
 
-	n, err := db.Import(rdf, importBatchSize)
-	if err != nil {
-		log.Fatal(err)
+	if *dump {
+		if err := db.Dump(os.Stdout); err != nil {
+			log.Fatal(err)
+		}
 	}
-	log.Printf("Imported %d triples from %s", n, *importF)
 }
